b5e78b8fa3e1b28c56425ae769ca492c3df10470
Esther Alter <subalterngames@gmail.com>
2024-12-29 13:13:04 -0500

    thats all for now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use cosmic_text::{Attrs, Buffer, FontSystem, Metrics, Shaping};
+use md::to_rtf;
 
+mod md;
 mod page;
 pub mod prelude;
 
@@ -7,14 +9,16 @@ fn main() {
     let mut font_system = FontSystem::new();
     let metrics = Metrics::new(14.0, 20.0);
 
+    let rtf = to_rtf(include_str!("test.md"));
+
     // A Buffer provides shaping and layout for a UTF-8 string, create one per text widget
     let mut buffer = Buffer::new(&mut font_system, metrics);
     buffer.set_size(&mut font_system, Some(500.), None);
     // Attributes indicate what font to choose
     let attrs = Attrs::new();
-    buffer.set_text(
+    buffer.set_rich_text(
         &mut font_system,
-        include_str!("lorem.txt"),
+        rtf.iter().map(|(s, a)| (s.as_str(), *a)),
         attrs,
         Shaping::Advanced,
     );
