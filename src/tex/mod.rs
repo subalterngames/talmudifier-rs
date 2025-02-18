e5527af58149be69ac6ddb04c96a33ee241caf92
Esther Alter <alters@mit.edu>
2025-02-18 11:17:29 -0500

    ok

--- src/tex/mod.rs
+++ src/tex/mod.rs
@@ -8,13 +8,15 @@ use error::Error;
 use page::Page;
 use pdf_extract::extract_text_from_mem;
 use position::Position;
-use rayon::iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
+use rayon::iter::{
+    IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator, ParallelIterator,
+};
 use table::Table;
 use tectonic::latex_to_pdf;
 
 use crate::{
     column::width::Width,
-    font::{self, tex_font::TexFont, tex_fonts::TexFonts},
+    font::tex_fonts::TexFonts,
     word::Word,
 };
 
@@ -33,36 +35,44 @@ pub struct Tex<P: AsRef<Path>> {
 }
 
 impl<P: AsRef<Path>> Tex<P> {
-    pub fn get_table(
+    pub fn get_min_lines(
         &self,
         left: &[Word],
         center: &[Word],
         right: &[Word],
         table: Table,
-    ) -> Result<String, Error> {
+    ) -> Result<usize, Error> {
+        // Get each font command.
+        let commands = [&self.fonts.left, &self.fonts.center, &self.fonts.right]
+            .iter()
+            .map(|f| f.command.clone())
+            .collect::<Vec<String>>();
+        // Get the preamble so that it can be shared between threads.
+        let preamble = self.preamble.clone();
         // Get the column with the least words.
         match [left, center, right]
             .into_par_iter()
-            .zip([Position::Left, Position::Center, Position::Right])
-            .zip([&self.fonts.left, &self.fonts.center, &self.fonts.right])
-            .filter_map(|(w, p, f)| {
+            .zip(
+                [Position::Left, Position::Center, Position::Right]
+                    .into_par_iter()
+                    .zip(commands),
+            )
+            .filter_map(|(w, (p, c))| {
                 if w.is_empty() {
                     None
                 } else {
                     let width = table.get_width(&p);
-                    Some(Self::get_num_lines(&self.preamble, w, &f.command, width).unwrap())
+                    Some(Self::get_num_lines(&preamble, w, &c, width).unwrap())
                 }
             })
             .min()
         {
-            Some(min_num_lines) => {
-                // TODO start indices.
-            }
+            Some(min_num_lines) => Ok(min_num_lines),
             None => Err(Error::MinNumLines),
         }
     }
 
-    fn get_words(
+    pub fn get_words(
         preamble: &str,
         font_command: &str,
         words: &[Word],
