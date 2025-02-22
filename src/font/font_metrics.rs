13c66b8838aa34633122e479bd73286d093d0931
Esther Alter <subalterngames@gmail.com>
2025-02-22 09:56:49 -0500

    oops

---
+++ src/font/font_metrics.rs
@@ -0,0 +1,23 @@
+use cosmic_text::Metrics;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct FontMetrics {
+    pub size: f32,
+    pub skip: f32,
+}
+
+impl Default for FontMetrics {
+    fn default() -> Self {
+        Self {
+            size: 11.,
+            skip: 13.,
+        }
+    }
+}
+
+impl From<&FontMetrics> for Metrics {
+    fn from(value: &FontMetrics) -> Self {
+        Metrics::new(value.size, value.skip)
+    }
+}
