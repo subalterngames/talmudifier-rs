34b22f6309b95e1f8d5eb84728a1f89c9991aa67
Esther Alter <subalterngames@gmail.com>
2025-04-04 09:41:23 -0400

    here we go again

--- src/font/mod.rs
+++ src/font/mod.rs
@@ -1,3 +1,6 @@
+use cosmic_font::CosmicFont;
+use tex_font::TexFont;
+
 pub mod cosmic_font;
 #[cfg(feature = "default-fonts")]
 mod default_fonts;
@@ -7,3 +10,8 @@ pub mod font_metrics;
 pub mod font_paths;
 pub mod tex_font;
 pub mod tex_fonts;
+
+pub struct Font {
+    pub cosmic: CosmicFont,
+    pub tex: TexFont
+}
---
+++ src/typesetter/table/span_column.rs
@@ -0,0 +1,18 @@
+use crate::{font::cosmic_font::CosmicFont, typesetter::span::Span};
+
+/// A column of text that can be typeset.
+/// Columns try to fill a target number of lines with words.
+/// Cosmic is used to get an initial guess at the number of words.
+/// Then, Tectonic is used to fill the column.
+///
+/// `SpanColumn` has a `start` index that are continuously re-sliced for typesetting.
+pub struct SpanColumn {
+    /// All of the words in the column.
+    span: Span,
+    /// The start index of the `words` slice.
+    start: usize,
+    /// The font used in Cosmic.
+    cosmic_font: CosmicFont,
+    /// The command to set the TeX font.
+    tex_font: String,
+}
