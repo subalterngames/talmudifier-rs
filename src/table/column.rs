10d32c2a663ffb03869163ef3cfb3338c210b783
Esther Alter <subalterngames@gmail.com>
2025-04-09 14:14:32 -0400

    column test

--- src/table/column.rs
+++ src/table/column.rs
@@ -2,25 +2,38 @@ use super::{maybe_span_column::MaybeSpanColumn, width::Width};
 
 /// A column, possibly with text, and a width.
 pub enum Column<'t> {
+    /// Typeset as if there is a column, even if there isn't any text.
     Column {
         column: MaybeSpanColumn<'t>,
         width: Width,
     },
+    /// Ignore this column when typesetting.
     None,
 }
 
 macro_rules! column_width {
     ($func_name:ident, $width:expr) => {
         pub fn $func_name(column: MaybeSpanColumn<'t>) -> Column<'t> {
-            Column::Column {
-                column,
-                width: $width,
-            }
+            Column::new(column, $width)
         }
     };
 }
 
 impl<'t> Column<'t> {
+    pub fn new(column: MaybeSpanColumn<'t>, width: Width) -> Self {
+        match &column {
+            MaybeSpanColumn::Span(span_column) => {
+                // If there are no more words, ignore the column.
+                if span_column.done() {
+                    Self::None
+                } else {
+                    Self::Column { column, width }
+                }
+            }
+            MaybeSpanColumn::Empty => Self::Column { column, width },
+        }
+    }
+
     column_width!(third, Width::Third);
 
     column_width!(half, Width::Half);
@@ -29,13 +42,47 @@ impl<'t> Column<'t> {
 
     column_width!(one, Width::One);
 
+    /// Returns true if there are more words to add to the page.
     pub fn done(&self) -> bool {
-        match self {
-            Self::Column { column, width: _ } => match column {
-                MaybeSpanColumn::Span(column) => column.done(),
-                MaybeSpanColumn::Empty => true,
-            },
-            Self::None => true,
-        }
+        matches!(self, Self::None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        font::cosmic_font::CosmicFont,
+        span::Span,
+        table::{maybe_span_column::MaybeSpanColumn, span_column::SpanColumn, width::Width},
+    };
+
+    use super::Column;
+
+    #[test]
+    fn test_column() {
+        let column = Column::None;
+        assert!(column.done());
+
+        let empty = MaybeSpanColumn::Empty;
+        let column = Column::new(empty, Width::Half);
+        assert!(!column.done());
+
+        let span = Span::from_md("There are so many words in this sentence!").unwrap();
+        let mut span_column = SpanColumn::new(span, CosmicFont::default_left(), "\\font");
+        span_column.start = 3;
+        let full = MaybeSpanColumn::Span(&mut span_column);
+        let column = Column::Column {
+            column: full,
+            width: Width::Half,
+        };
+        assert!(!column.done());
+
+        let span = Span::from_md("There are so many words in this sentence!").unwrap();
+        let len = span.0.len();
+        let mut span_column = SpanColumn::new(span, CosmicFont::default_left(), "\\font");
+        span_column.start = len;
+        let full = MaybeSpanColumn::Span(&mut span_column);
+        let column = Column::new(full, Width::Half);
+        assert!(column.done());
     }
 }
