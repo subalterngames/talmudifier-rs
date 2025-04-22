15816ae10dac4bd979526f1f947af688781da5c4
Esther Alter <subalterngames@gmail.com>
2025-04-21 20:25:31 -0400

    removed unused errors

--- src/error.rs
+++ src/error.rs
@@ -4,23 +4,16 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[cfg(target_os = "windows")]
-    #[error("Failed to create a PDF. Talmudifier cannot create PDFs on Windows.")]
-    Pdf,
     #[cfg(target_os = "linux")]
     #[error("Error creating PDF: {0}")]
     Pdf(tectonic::Error),
     #[error("Error extracting text from PDF: {0}")]
     #[cfg(target_os = "linux")]
     Extract(pdf_extract::OutputError),
-    #[error("Failed to get the minimum number of lines: {0}")]
-    MinNumLines(String),
     #[error("Markdown parsing error: {0}")]
     Md(markdown::message::Message),
     #[error("Tried to create a table but there are no words.")]
     NoMoreWords,
-    #[error("Error loading font: {0}")]
-    Font(io::Error),
     #[error("Cosmic font error: {0}")]
     CosmicFont(io::Error),
     #[error("Font not found: {0}")]
